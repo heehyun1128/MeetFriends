85a21eb0fa191649239e0aa6cb76b25c6047eca8
heehyun1128 <chenyiapp@yahoo.com>
2023-08-25 16:29:15 -0700

    refactored code to close down nested window after clicking on login or signup

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef } from "react";
 import { useDispatch } from 'react-redux';
 import * as sessionActions from '../../store/session';
-import OpenModalButton from '../OpenModalButton';
+import OpenModalMenuItem from './OpenModalMenuItem';
 import LoginFormModal from '../LoginFormModal';
 import SignupFormModal from '../SignupFormModal';
 import './Navigation.css';
@@ -30,9 +30,12 @@ function ProfileButton({ user }) {
     return () => document.removeEventListener("click", closeMenu);
   }, [showMenu]);
 
+  const closeMenu = () => setShowMenu(false);
+
   const logout = (e) => {
     e.preventDefault();
     dispatch(sessionActions.logout());
+    closeMenu();
   };
 
   const ulClassName = "profile-dropdown" + (showMenu ? "" : " hidden");
@@ -54,20 +57,18 @@ function ProfileButton({ user }) {
             </li>
           </>
         ) : (
-          <>
-            <li>
-              <OpenModalButton
-                buttonText="Log In"
+            <>
+              <OpenModalMenuItem
+                itemText="Log In"
+                onItemClick={closeMenu}
                 modalComponent={<LoginFormModal />}
               />
-            </li>
-            <li>
-              <OpenModalButton
-                buttonText="Sign Up"
+              <OpenModalMenuItem
+                itemText="Sign Up"
+                onItemClick={closeMenu}
                 modalComponent={<SignupFormModal />}
               />
-            </li>
-          </>
+            </>
         )}
       </ul>
     </div>
--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -10,30 +10,6 @@ import "./Navigation.css";
 function Navigation({ isLoaded }) {
   const sessionUser = useSelector(state => state.session.user);
 
-  // let sessionLinks;
-  // if (sessionUser) {
-  //   sessionLinks = (
-  //     <li>
-  //       <ProfileButton user={sessionUser} />
-  //     </li>
-  //   );
-  // } else {
-  
-  //   sessionLinks = (
-  //     <li>
-  //       <OpenModalButton
-  //         buttonText="Log In"
-  //         modalComponent={<LoginFormModal />}
-  //       />
-  //       <OpenModalButton
-  //         buttonText="Sign Up"
-  //         modalComponent={<SignupFormModal />}
-  //       />
-  //     </li>
-  //   );
-  // }
-
-
   return (
     <ul id='nav-div'>
       <li >
