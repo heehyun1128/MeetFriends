f2e66c5820c91826de86865e1c4a5b62ca8827d0
heehyun1128 <chenyiapp@yahoo.com>
2023-09-01 19:13:11 -0700

    fixed bugs for routing to 404 page when group is not found for creating event

--- frontend/src/components/Event/EventForm/CreateEventForm.js
+++ frontend/src/components/Event/EventForm/CreateEventForm.js
@@ -1,31 +1,34 @@
 import EventForm from "./EventForm";
 import { useEffect, useState } from 'react';
 import { useHistory, useParams } from 'react-router-dom'
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchGroups } from "../../../store/group";
+import { useDispatch } from 'react-redux';
+// import { fetchGroups } from "../../../store/group";
+import { fetchGroupDetail } from '../../../store/group';
 const CreateEventForm = () => {
   const {groupId}=useParams()
   const history = useHistory()
   const dispatch = useDispatch();
   const [validationError, setValidationError] = useState({})
 
-  // get all groups
-  const groupObj = useSelector((state) => (Object.values(state.groups.allGroups).length ? state.groups.allGroups : {}))
-  const groupInfo = groupObj[groupId]
+
+
   useEffect(() => {
-    dispatch(fetchGroups()).catch(
+    dispatch(fetchGroupDetail(groupId)).catch(
       async (res) => {
         const data = await res.json();
-        //  console.log(data)
+         console.log(data)
+        console.log(data.message)
         if (data && data.message) {
-          setValidationError(data.message);
+          setValidationError(data);
         }
-
+       
       }
-    )
-  }, [dispatch]);
+      )
+    }, [dispatch, groupId]);
+
+    
  
-  if (Object.values(validationError).length) {
+  if (Object.values(validationError).length ) {
     history.push('/404')
 
   }
